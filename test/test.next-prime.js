3a98a823149be4f9e56458e35940a1a7481f2c67
someone <yukoga@gmail.com>
2013-02-04 20:14:01 +0900

    first commit

---
+++ test/test.next-prime.js
@@ -0,0 +1,27 @@
+var assert = require('assert'),
+	nextPrime = require('./../index').nextPrime,
+	asyncPrime = require('./../index').asyncPrime;
+
+
+suite('nextPrime', function(){
+	test('nextPrime should return the next prime numeber', function(done) {
+		assert.equal(11, nextPrime(7));	
+		done();
+	});
+
+	test('zero and one are not prime numbers', function(){
+		assert.equal(2, nextPrime(0));
+		assert.equal(2, nextPrime(1));
+	});
+
+});
+
+
+suite('asyncPrime', function() {
+	test('asyncPrime should return the next prime number', function(done) {
+		asyncPrime(128, function(n) {
+			assert.equal(131, n, "Wrong number");
+			done();
+		});
+	});
+});
